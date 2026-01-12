a9f9c08293b0811dab6dcfeb5e17ce40ae88ec92
Soaritalo <italoantoniosj@gmail.com>
2026-01-12 20:56:29 -0300

    subtraçao

---
+++ caculadora multiplicacao.py
@@ -0,0 +1,5 @@
+print('Bem vindo a calculadora de multiplicacao!')
+numero1 = float(input('Digite o primeiro numero da multiplicacao'))
+numero2 = float(input('Digite o segundo numero da multiplicacao'))
+resultado = numero1*numero2
+print('Esse é o seu resultado: ' ,resultado)
---
+++ calculadora subtracao.py
@@ -0,0 +1,4 @@
+numero1 = float(input('digite o  numerador : '))
+numero2 = float(input('digite o denominador (obs o valor nao pode ser igual a 0) : '))
+resultado = numero1/numero2
+print(float(resultado))
