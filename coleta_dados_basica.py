80021da6ef5f28de7c7a57f6e75c355776dd528a
Soaritalo <italoantoniosj@gmail.com>
2026-01-12 16:32:20 -0300

    Create coleta_dados_basica.py
    Script em Python para coleta e conversão de dados básicos via input(), desenvolvido como estudo em Python para Dados (Alura).

---
+++ coleta_dados_basica.py
@@ -0,0 +1,9 @@
+nome = input('Digite seu nome:  ')
+idade = int(input('Digite sua idade: '))
+altura = float(input('Digite sua altura em metros:  '))
+
+print(f'Olá {nome}')
+print(f'você tem {idade} anos')
+print(f'você tem {altura} de altura ')
+
+input('Pressione enter para sair.')
