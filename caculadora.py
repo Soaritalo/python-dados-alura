bd6fc8b0676a9df3409023e6a575082706c458d4
Soaritalo <italoantoniosj@gmail.com>
2026-01-12 20:38:38 -0300

    calculadora

---
+++ caculadora.py
@@ -0,0 +1,7 @@
+print('bem vindo a Calculadora')
+numero1 = float(input('digite o primeiro numero:'))
+numero2 = float(input('digite o segundo numero: '))
+numero3 = float(input('digite o terceiro numero: '))
+resultado = numero1 + numero2 + numero3
+print("seu numero é: ", resultado)
+input('pressione enter para sair')
