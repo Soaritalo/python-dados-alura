75c0d07d6c5e60dac1af1383f97d061a2a6c97b4
Soaritalo <italoantoniosj@gmail.com>
2026-01-27 15:35:42 -0300

    Add number guessing game in for.py

---
+++ for.py
@@ -0,0 +1,11 @@
+for numeros in range (0,10):
+    tentativa = int(input('Tente acertar o numero correto amiguinho!!: '))
+    if tentativa == 10:
+        print("vc acertou o numero!")
+        print(f"(em apenas:{numeros} tentativas)")
+        input('aperte enter para sair ')
+        break
+    elif tentativa > 10 or tentativa < 10 :
+        print('vc errou amiguinho')
+        continue
+    
