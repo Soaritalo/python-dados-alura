ab338c9336765c10a33141c3e2729e2a863360d9
Soaritalo <italoantoniosj@gmail.com>
2026-01-27 15:40:10 -0300

    Change range start from 0 to 1 in for loop
    botei de 1 pra 10 pra ficar melhor na hora de ver as tentativas feitas

--- for.py
+++ for.py
@@ -1,4 +1,4 @@
-for numeros in range (0,10):
+for numeros in range (1,10):
     tentativa = int(input('Tente acertar o numero correto amiguinho!!: '))
     if tentativa == 10:
         print("vc acertou o numero!")
