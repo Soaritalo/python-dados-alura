46cdc3bc178d0f1200316008ef137144c7432db0
Soaritalo <italoantoniosj@gmail.com>
2026-02-06 15:10:51 -0300

    Add Lista.py to calculate sum and average of a list

---
+++ dados/Lista.py
@@ -0,0 +1,9 @@
+lista = [2172.54, 3701.35, 3518.09, 3456.61, 3249.38,
+         2840.82, 3891.45, 3075.26, 2317.64, 3219.08]
+soma = sum(lista)
+quantidade= len(lista)
+print(f'{quantidade}')
+print(f'{soma}')
+media = soma/quantidade
+print(f'{media}')
+input('aperte')
